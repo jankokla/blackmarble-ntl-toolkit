d15b656e2b7d404157cdbe2a034da58ed304cee4
Jan Kokla <jan.kokla@gmail.com>
2026-06-01 17:23:41 +0200

    update environment variable logic

--- src/blackmarble_toolkit/utils.py
+++ src/blackmarble_toolkit/utils.py
@@ -7,15 +7,15 @@ def initialize_ee(project_name: str | None = None) -> None:
     Attempts to initialize Earth Engine. Falls back to authentication if required.
 
     Args:
-        project_name: The GCP project name for EE initialization. If None,
-            it attempts to read the 'EE_PROJECT' environment variable.
+        project_name: Optional explicit project name. If not provided,
+            it attempts to read the 'GOOGLE_CLOUD_PROJECT' environment variable.
     """
-    project = project_name or os.environ.get("EE_PROJECT")
+    project = project_name or os.environ.get("GOOGLE_CLOUD_PROJECT")
 
     if not project:
         raise ValueError(
             "Earth Engine project name not provided. Pass it to the function "
-            "or set the 'EE_PROJECT' environment variable."
+            "or set the 'GOOGLE_CLOUD_PROJECT' environment variable."
         )
 
     try:
